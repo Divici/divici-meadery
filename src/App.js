8fce8c52477a0e5ab1960f16db9455a75618ada3
david-aihe <doa9200@gmail.com>
2022-01-22 23:43:35 -0500

    Finished banner styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './assets/DiviciMeaderyLogoB.png';
 
+
+
 function App() {
   return (
     <div className="App">
@@ -20,9 +22,12 @@ function App() {
           <p>Welcome to Divici's online mead resource. Here you will be able to find the wide range of experimental meads as well as refined meads I have created over the last few years. It is also just a place to track what I'm currently working on as well as leave some tips on how to recreate my recipes.</p>
           <button>See All</button>
         </div>
-        <div className='float-right'>
-          
-        </div>
+        <div className='float-right'></div>
+      </div>
+
+      <div className='best-header'>
+        <p>Check out some of my work</p>
+        <h2>Best Finished Meads</h2>
       </div>
       
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,900&display=swap');
+</style>
 
 ReactDOM.render(
   <React.StrictMode>
