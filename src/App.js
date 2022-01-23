30de1ffa4475e481c79c94807d83663ff36f9efd
david-aihe <doa9200@gmail.com>
2022-01-22 21:48:53 -0500

    Banner and Header DOM added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/DiviciMeaderyLogoB.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="logo">
+          <img src={logo} alt='logo'></img>
+        </div>
+        <div className="nav">
+          <a>In Progress</a>
+          <a>Finished Meads</a>
+          <a>Contact Me</a>
+        </div>
       </header>
+      
+      <div className='banner'>
+        <div className='float-text'>
+          <h1>Distinguished<br></br>Meads</h1>
+          <p>Welcome to Divici's online mead resource. Here you will be able to find the wide range of experimental meads as well as refined meads I have created over the last few years. It is also just a place to track what I'm currently working on as well as leave some tips on how to recreate my recipes.</p>
+          <button>See All</button>
+        </div>
+        <div className='float-right'>
+          
+        </div>
+      </div>
+      
     </div>
   );
 }
