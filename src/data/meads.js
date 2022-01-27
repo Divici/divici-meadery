4382cea84bd23f9dc09c9601233058c52ca7c496
david-aihe <doa9200@gmail.com>
2022-01-26 19:46:46 -0500

    created mead data file

---
+++ src/data/meads.js
@@ -0,0 +1,36 @@
+
+const meads = [
+    {
+        id: 1,
+        info: {
+            name: '',
+            size: 0,
+            honey: [],
+            yeast: '',
+        },
+        primary:    {
+            start: '',
+            end: '',
+            gravityStart: 0,
+            gravityEnd: 0,
+            ingredients: [],
+            details: ''
+        },
+        secondary: {
+            start: '',
+            end: '',
+            gravityStart: 0,
+            gravityEnd: 0,
+            ingredients: [],
+            details: ''
+        },
+        aging: {
+            bottle: '',
+            date: '',
+            age: ''
+        }
+    }
+
+];
+
+export default meads;
