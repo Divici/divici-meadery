85d0d5afab55eefd6cd2b377435056d6ebb25cfb
david-aihe <doa9200@gmail.com>
2022-06-16 00:12:41 -0400

    Started About Us section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {images} from '../../constants'
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='section-padding central relative bg-gradient-to-br from-red-dk to-red-br'>
+    <div className="my-8 mx-16 central">
+        <img src={images.meadBottle} alt="about_bottle" className='h-[910px]'/>
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SpecialMenu = () => (
-  <div>
+  <div className='h-screen bg-cream-md'>
     SpecialMenu
   </div>
 );
